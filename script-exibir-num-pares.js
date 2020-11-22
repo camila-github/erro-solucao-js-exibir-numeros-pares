99b254a38f8d20ac8c2c1303b721b66516d6b2b5
Camila Augusta dos Santos <camilaaugustasantos@yahoo.com.br>
2020-11-22 04:02:33 -0300

    ExibirNumerosPares2

--- script-exibir-num-pares.js
+++ script-exibir-num-pares.js
@@ -1,6 +1,6 @@
 //SOLUÇÃO 1
-var valor = parseInt(gets()); //Leitura da entrada 
-for (var i = 0; i <= valor; i++) {
+var num = parseInt(gets()); //Leitura da entrada 
+for (var i = 0; i <= num; i++) {
     if (i % 2 == 0) { //verifica se o resto da divisão 
         if (i != 0) { //verifica se o numero é diferente de 0, para nao ser mostrado
             console.log(i);
@@ -10,8 +10,7 @@ for (var i = 0; i <= valor; i++) {
 
 
 //SOLUÇÃO 2 - Resumido
-/* No if verifica se o resto da divisão é 0.
-e verifica se o numero é diferente de 0, para nao ser mostrado no console*/
+/* Na estrutura de repetição (for) o i começa com 1, para não mostrar o zero no console*/
 num = parseInt(gets());
-for (i = 0; i <= num; i++)
-    if (i % 2 == 0 && i != 0) console.log(i);
+for (i = 1; i <= num; i++)
+    if (i % 2 == 0) console.log(i);
